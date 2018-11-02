b58fda01b98fa4184822f1e5ae6d98ba55434a68
Bashir <harrell.bashir@yahoo.com>
2018-11-02 18:13:59 +0800

    Added https://cors-anywhere.herokuapp.com/ to getJSON in scripts.js to allow for cross origin http -> https

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var colors = ["#50d143", "#a868a1", "#d43450","#750d14", "#ea1a28", "#3a868a", "#0750d1", "#83a868", "#c1d434", "#f0750d", "#783a86", "#c783a8", "#63c1d4", "#6158f0", "#30ac78", "#e9a4c2"];
   $("#getQuote").on("click", function() {
    //gets api and appends to html elements
-    $.getJSON("http://quotes.stormconsultancy.co.uk/random.json?dataType='jsonp'&type='GET'", function(json) {
+    $.getJSON("https://cors-anywhere.herokuapp.com/http://quotes.stormconsultancy.co.uk/random.json?dataType='jsonp'&type='GET'", function(json) {
       var randomQuoteString = json.quote;
       var authorString = JSON.stringify(json.author);
       $(".message").html(JSON.stringify(json.quote));
